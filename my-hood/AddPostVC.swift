6f9483f82b1ed923d33da9c5db110c4b5a34f6f4
Michael Mahon <michael.mahon@live.com>
2016-05-17 22:50:21 -0400

    Allow user to add posts with images and save them to device storage. Load the posts when the app is opened.

---
+++ my-hood/AddPostVC.swift
@@ -0,0 +1,53 @@
+//
+//  AddPostVC.swift
+//  my-hood
+//
+//  Created by Michael Mahon on 5/17/16.
+//  Copyright © 2016 Michael Mahon. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descField: UITextField!
+    
+    var imgPicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        postImg.layer.cornerRadius = postImg.frame.size.width / 2
+        postImg.clipsToBounds = true
+        imgPicker = UIImagePickerController()
+        imgPicker.delegate = self
+        
+    }
+
+    @IBAction func makePostBtnPressed(sender: AnyObject) {
+        if let title = titleField.text, let desc = descField.text, let img = postImg.image {
+            
+            let imgPath = DataService.instance.saveImgandCreatePath(img)
+            
+            let post = Post(imagePath: imgPath, title: title, description: desc)
+            DataService.instance.addPost(post)
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func addPicBtnPressed(sender: UIButton!) {
+        sender.setTitle("", forState: .Normal)
+        presentViewController(imgPicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imgPicker.dismissViewControllerAnimated(true, completion: nil)
+        postImg.image = image
+    }
+}
---
+++ my-hood/DataService.swift
@@ -0,0 +1,72 @@
+//
+//  DataService.swift
+//  my-hood
+//
+//  Created by Michael Mahon on 5/17/16.
+//  Copyright © 2016 Michael Mahon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DataService {
+    static let instance = DataService()
+    
+    let KEY_POSTS = "posts"
+    private var _loadedPosts = [Post]()
+    
+    var loadedPosts: [Post] {
+        return _loadedPosts
+    }
+    
+    func savePosts() {
+        let postsData = NSKeyedArchiver.archivedDataWithRootObject(loadedPosts)
+        NSUserDefaults.standardUserDefaults().setObject(postsData, forKey: KEY_POSTS)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func loadPosts() {
+        if let postsData = NSUserDefaults.standardUserDefaults().objectForKey(KEY_POSTS) as? NSData {
+            
+            if let postsArray = NSKeyedUnarchiver.unarchiveObjectWithData(postsData) as? [Post] {
+                _loadedPosts = postsArray
+            }
+        }
+        
+        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "postsLoaded", object: nil))
+    }
+    
+    func saveImgandCreatePath(img: UIImage) -> String {
+        let imgData = UIImagePNGRepresentation(img)
+        let imgPath = "image\(NSDate.timeIntervalSinceReferenceDate()).png"
+        let fullPath = documentsPathForFileName(imgPath)
+        imgData?.writeToFile(fullPath, atomically: true)
+        return imgPath
+    }
+    
+    func imageForPath(path: String) -> UIImage? {
+        let fullPath = documentsPathForFileName(path)
+        let img = UIImage(named: fullPath)
+        return img
+    }
+    
+    func addPost(post: Post) {
+        _loadedPosts.append(post)
+        savePosts()
+        loadPosts()
+    }
+    
+    func deleteAllPosts() {
+        _loadedPosts = []
+        savePosts()
+        loadPosts()
+    }
+    
+    
+    func documentsPathForFileName(name: String) -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let fullPath = paths[0] as NSString
+        return fullPath.stringByAppendingPathComponent(name)
+    }
+    
+}
